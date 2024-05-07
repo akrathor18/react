e2e604fafefe6a88b59eef55beb80829d48aab6b
akrathor18 <akrathor18710@gmail.com>
2024-05-07 19:17:36 +0530

    Commit

--- 06ReactRouter/src/App.jsx
+++ 06ReactRouter/src/App.jsx
@@ -1,15 +1,17 @@
 import { useState } from 'react'
+import { Link, Outlet } from 'react-router-dom';
+import Header from './components/Header/Header'
 import './App.css'
+import Footer from './components/footer/Footer'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-   <>
-   <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-   </>
+  <>
+  <Header/>
+  <Outlet/>
+  <Footer/>
+  </>
   )
 }
 
--- 06ReactRouter/src/main.jsx
+++ 06ReactRouter/src/main.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
+
+import App from './App.jsx'
+import Home from './components/Home/Home.jsx'
+import About from './components/About/About.jsx'
+import Contact from './Contact/Contact.jsx'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App />}>
+      <Route path='' element={<Home />} />
+      <Route path='about' element={<About />} />
+      <Route path='contact' element={<Contact />} />
+      {/* <Route path='user/:userid' element={<User />} /> */}
+      {/* <Route 
+      loader={githubInfoLoader}
+      path='github' 
+      element={<Github />}
+       /> */}
+    </Route>
+  )
+)
+  
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <React.StrictMode>   
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
