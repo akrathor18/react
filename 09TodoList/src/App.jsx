054a2f137b778b24a781f1f6093d0fcd00681211
akrathor18 <akrathor18710@gmail.com>
2024-05-11 20:07:57 +0530

    Commit

--- 09TodoList/src/App.jsx
+++ 09TodoList/src/App.jsx
@@ -1,12 +1,40 @@
 import { useState } from 'react'
 import './App.css'
+import { TodoProvider } from './Context'
+import TodoForm from './Components/TodoForm'
+import TodoItem from './Components/TodoItem'
 
 function App() {
+const [todos, setTodos]= useState([])
+
+const addTodo=(Todo)=>{
+  setTodos(  (prev)=>[{id:Date.now(),...todo},...prev])
+}
+
+const updateTodo = (id, todo) => {
+  setTodos((prev) => prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo )))
+
+  
+}
 
   return (
-    <>
+    <TodoProvider value={{todos, deleteTodo, updateTodo,toggleComplete,addTodo}}>
     <h1>hello world</h1>
-    </>
+
+    <div className="bg-[#172842] min-h-screen py-8">
+                <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+                    <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
+                    <div className="mb-4">
+                        {/* Todo form goes here */} 
+                        {/* <TodoForm/> */}
+                    </div>
+                    <div className="flex flex-wrap gap-y-3">
+                        {/*Loop and Add TodoItem here */}
+                        {/* <TodoItem/> */}
+                    </div>
+                </div>
+            </div>
+    </TodoProvider>
   )
 }
 
