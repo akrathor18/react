bab23a88fbf1ee624d18db33e0d9b470c75a42f2
akrathor18 <akrathor18710@gmail.com>
2024-05-27 20:06:43 +0530

    inntal ocommit

--- routeProject/src/App.jsx
+++ routeProject/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useRef, useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Nav from "./components/Nav";
@@ -7,12 +7,22 @@ import { Footer } from "./components/Footer";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import NotFound from "./components/NotFound";
-
 function App() {
+//   const ref= useRef()
+//  const [count, setCount] = useState(0)
+//  useEffect(() => {
+//    console.log(ref.current);
+//  },
+
+const [show, setShow] = useState(false)
+ 
   return (
-    <>
-     <BrowserRouter>
+    <> 
+    <BrowserRouter>
      <Nav/>
+     {show&& <Contact/>}
+
+    <button onClick={()=>{setShow(!show)}} >Click me to show contact button</button>
      <Routes>
       <Route path="/"  element={<HeroOne/>}/>
       <Route path="/about" element={<About/>}/>
