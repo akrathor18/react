11c4aebb8781a5c0226f31b732faa33f95c9df0d
akrathor18 <akrathor18710@gmail.com>
2024-06-07 15:16:59 +0530

    innial commit

--- Redux/src/redux/counterSlice.js
+++ Redux/src/redux/counterSlice.js
@@ -16,7 +16,7 @@ const counterSlice = createSlice({
             state.value -= 1;
         },
         incrementByAmount: (state, action) => {
-            state.value += action.payload;
+            state.value += action.payload
         },
     },
 });
